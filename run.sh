2f0b9845813e6a875c91c22b89716eb5fca55f8a
ryokatsu <ryokatsu0719@gmail.com>
2023-10-01 22:28:35 +0900

    feat:kernel.cの実装

--- run.sh
+++ run.sh
@@ -5,5 +5,15 @@ set -xue
 # QEMU_PATH=/usr/local/bin/qemu-system-x86_64
 QEMU=qemu-system-riscv32
 
-# QMENUを起動
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
+# clangのパス (Ubuntuの場合は CC=clang)
+CC=/opt/homebrew/opt/llvm/bin/clang
+
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
+
+# カーネルをビルド
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    kernel.c
+
+# QEMUを起動
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+    -kernel kernel.elf
